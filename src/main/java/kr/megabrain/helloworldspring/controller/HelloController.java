bba2f1b69c16650bae2c5fb06f02fa73e4ee753e
Younggun Park <ygpark0922@gmail.com>
2022-02-21 18:20:18 +0900

    first commit

---
+++ src/main/java/kr/megabrain/helloworldspring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package kr.megabrain.helloworldspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello jadru!!");
+        return ("hello");
+    }
+}
