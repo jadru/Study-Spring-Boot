0e51f70ccc9751504cfd43729bab377d35fc179e
Younggun Park <ygpark0922@gmail.com>
2022-02-21 23:08:22 +0900

    member repo and test code upgrade

---
+++ src/main/java/kr/megabrain/helloworldspring/service/MemberService.java
@@ -0,0 +1,34 @@
+package kr.megabrain.helloworldspring.service;
+
+import kr.megabrain.helloworldspring.domain.Member;
+import kr.megabrain.helloworldspring.repository.MemberRepository;
+import kr.megabrain.helloworldspring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+        validateExistMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateExistMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
--- src/test/java/kr/megabrain/helloworldspring/HelloWorldSpringApplicationTests.java
+++ src/test/java/kr/megabrain/helloworldspring/HelloWorldSpringApplicationTests.java
@@ -1,13 +1,68 @@
 package kr.megabrain.helloworldspring;
 
+import kr.megabrain.helloworldspring.domain.Member;
+import kr.megabrain.helloworldspring.repository.MemberRepository;
+import kr.megabrain.helloworldspring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class HelloWorldSpringApplicationTests {
 
+	MemoryMemberRepository repository = new MemoryMemberRepository();
+
+	@AfterEach
+	public void afterEach() {
+		repository.clearStore();
+	}
+
 	@Test
-	void contextLoads() {
+	public void save() {
+		Member member = new Member();
+		member.setName("spring");
+
+		repository.save(member);
+
+		Member result = repository.findById(member.getId()).get();
+		assertEquals(member, result);
+	}
+
+	@Test
+	public void findByName() {
+		//given
+		Member member1 = new Member();
+		member1.setName("spring1");
+		repository.save(member1);
+
+		Member member2 = new Member();
+		member2.setName("spring2");
+		repository.save(member2);
+		//when
+		Member result = repository.findByName("spring1").get();
+
+		// then
+		assertEquals(member1, result);
+	}
+
+	@Test
+	public void findAll() {
+		//given
+		Member member1 = new Member();
+		member1.setName("spring1");
+		repository.save(member1);
+		Member member2 = new Member();
+		member2.setName("spring2");
+		repository.save(member2);
+		//when
+		List<Member> result = repository.findAll();
+		//then
+		assertEquals(2, result.size());
 	}
 
 }
